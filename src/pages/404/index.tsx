c08ea754c1cbade056a161bad5e603ae8dc54cb2
nurdaulet <scissxrd@gmail.com>
2024-10-08 15:06:55 +0500

    404

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,8 +1,23 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound: FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>404</div>
+    <div className='relative h-screen flex items-center justify-center'>
+      <button
+        onClick={() => navigate('/accompany')}
+        className='absolute top-5 left-5 bg-brand-200 text-white px-2 py-1 rounded'
+      >
+        На главную
+      </button>
+      <img
+        src="pages/404/404.jpg"
+        alt="404"
+        className='w-[50%]'
+      />
+    </div>
   )
 }
 
