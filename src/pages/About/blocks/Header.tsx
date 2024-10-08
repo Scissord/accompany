0d058d9dbba1c436059effb47fa7f2819a5d42e7
nurdaulet <scissxrd@gmail.com>
2024-10-08 18:20:36 +0500

    build

--- src/pages/About/blocks/Header.tsx
+++ src/pages/About/blocks/Header.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
 import { Navbar, NavbarMobile, HeaderSettings, HeaderLogo } from "@components";
+import './header.css';
 
 const Header: FC = () => {
   return (
-    <header className="sticky top-0 z-30 h-[8vh] lg:h-[40vh] bg-dbg text-white lg:relative lg:bg-[url('pages/about/plane.png')] lg:bg-cover lg:bg-bottom fijb lg:block">
+    <header className="header_about">
       <div className="hidden lg:block absolute inset-0 bg-brand-100 bg-opacity-70" />
       <div className="h-[8vh] container mx-auto hidden lg:fijb">
         <HeaderLogo />
